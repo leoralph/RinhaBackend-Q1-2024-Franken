709c06d715e71d9abb7d4449d1bdf230aa5d5468
Leonardo Ralph <theleoralph@gmail.com>
2024-02-08 10:42:09 -0300

    removendo inutilidades do sql e criando readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rinha de Backend Q1 2024
+
+Projeto criado para submissão na [Rinha de Backend](https://github.com/zanfranceschi/rinha-de-backend-2024-q1)
+
+Neste projeto não foram utilizadas quaisquer bibliotecas de composer, somente PHP puro e o webserver FrankenPHP, com banco de dados postgres e load balancer NGINX
+
+Para rodar basta clonar o projeto e utilizar o comando
+
+```bash
+docker compose up --build
+```
+As regras de execução estão detalhadas no próprio repositório da rinha
--- init.sql
+++ init.sql
@@ -1,14 +1,5 @@
-create table "users" ("id" bigserial not null primary key, "name" varchar(255) not null, "email" varchar(255) not null, "email_verified_at" timestamp(0) without time zone null, "password" varchar(255) not null, "remember_token" varchar(100) null, "created_at" timestamp(0) without time zone null, "updated_at" timestamp(0) without time zone null);
-alter table "users" add constraint "users_email_unique" unique ("email");
-create table "password_reset_tokens" ("email" varchar(255) not null, "token" varchar(255) not null, "created_at" timestamp(0) without time zone null);
-alter table "password_reset_tokens" add primary key ("email");
-create table "failed_jobs" ("id" bigserial not null primary key, "uuid" varchar(255) not null, "connection" text not null, "queue" text not null, "payload" text not null, "exception" text not null, "failed_at" timestamp(0) without time zone not null default CURRENT_TIMESTAMP);
-alter table "failed_jobs" add constraint "failed_jobs_uuid_unique" unique ("uuid");
-create table "personal_access_tokens" ("id" bigserial not null primary key, "tokenable_type" varchar(255) not null, "tokenable_id" bigint not null, "name" varchar(255) not null, "token" varchar(64) not null, "abilities" text null, "last_used_at" timestamp(0) without time zone null, "expires_at" timestamp(0) without time zone null, "created_at" timestamp(0) without time zone null, "updated_at" timestamp(0) without time zone null);
-create index "personal_access_tokens_tokenable_type_tokenable_id_index" on "personal_access_tokens" ("tokenable_type", "tokenable_id");
-alter table "personal_access_tokens" add constraint "personal_access_tokens_token_unique" unique ("token");
-create table "clients" ("id" bigserial not null primary key, "limite" integer not null, "saldo_inicial" bigint not null default '0', "saldo" bigint not null default '0');
+create table "clients" ("id" bigserial not null primary key, "limite" integer not null, "saldo" bigint not null default '0');
 create table "transactions" ("id" bigserial not null primary key, "client_id" bigint not null, "valor" bigint not null, "tipo" varchar(1) not null, "descricao" varchar(10) not null, "realizada_em" varchar(255) not null);
 alter table "transactions" add constraint "transactions_client_id_foreign" foreign key ("client_id") references "clients" ("id");
 create index "transactions_client_id_id_index" on "transactions" ("client_id", "id");
-insert into "clients" ("id", "limite", "saldo_inicial") values (1, 100000, 0), (2, 80000, 0), (3, 1000000, 0), (4, 10000000, 0), (5, 500000, 0) on conflict ("id") do update set "id" = "excluded"."id", "limite" = "excluded"."limite", "saldo_inicial" = "excluded"."saldo_inicial";
+insert into "clients" ("id", "limite") values (1, 100000), (2, 80000), (3, 1000000), (4, 10000000), (5, 500000) on conflict ("id") do update set "id" = "excluded"."id", "limite" = "excluded"."limite";
